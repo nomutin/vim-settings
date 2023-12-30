b847273c2fef818c1456ee9d383b97083887a447
nomutin <nomura0508@icloud.com>
2023-12-31 03:05:24 +0900

    :coffin: 不要なコードを削除

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ set -eu
 
 ln -sf "$HOME"/.vim/settings/.vimrc "$HOME"/.vimrc
 
-VIM_DIR=~/".vim"
-if [ ! -d $VIM_DIR ]; then
-  mkdir $VIM_DIR
-fi
+# VIM_DIR=~/".vim"
+# if [ ! -d $VIM_DIR ]; then
+#   mkdir $VIM_DIR
+# fi
 # ln -sf "$HOME"/.vim/settings/init "$HOME"/.vim/init
 # ln -sf "$HOME"/.vim/settings/pluginconfig "$HOME"/.vim/pluginconfig
