5479b4cc6eb6adf7c000037056e8ce841d05d86d
nomutin <nomura0508@icloud.com>
2023-12-31 02:58:29 +0900

    :bricks: リポジトリを`.vim`として直接’読み込むように修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,9 @@
 set -eu
 
 if [ ! -d vim-settings ]; then
-  git clone https://github.com/nomutin/vim-settings.git .vim-settings
+  git clone https://github.com/nomutin/vim-settings.git .vim
 fi
 
-cd .vim-settings
 make init
 make deploy
 
